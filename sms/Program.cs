1c364a91a5793d93902e397ac133675c9c5c0625
kocsist <ktomi87@gmail.com>
2020-09-26 14:46:37 +0200

    Add project files.

---
+++ sms/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace sms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            task1();
+        }
+
+        private static void task1()
+        {
+            Console.Write("Elso feladat\nKérek egy betűt: ");
+            string bekertBetu = Console.ReadLine();
+            string[] betuk = new string[] { "", "", "abc", "dfe", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz" };
+            for (int i = 0; i < betuk.Length; i++)
+            {
+                if (betuk[i].Contains(bekertBetu)) Console.WriteLine("Ez a kód tartozik hozzá: " + i);
+            }
+        }
+    }
+}
